6d5573243f8f8aef00cbbc2494251441f1847d76
currenjin <hyun0524e@naver.com>
2022-05-16 16:43:28 +0900

    update: elasticsearch url 환경변수로 수정

--- src/main/java/com/poc/elasticsearch/config/ElasticsearchConfig.java
+++ src/main/java/com/poc/elasticsearch/config/ElasticsearchConfig.java
@@ -1,6 +1,7 @@
 package com.poc.elasticsearch.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,8 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @ComponentScan(basePackages = "com.poc.elasticsearch.application")
 public class ElasticsearchConfig {
 
-    private static final String ELASTICSEARCH_URL = "localhost:9200";
+    @Value("${ELASTICSEARCH_URL}")
+    private String ELASTICSEARCH_URL;
 
     @Bean
     public RestHighLevelClient client() {
